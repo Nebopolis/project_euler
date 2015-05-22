1160f17c6510981bb230c67eeb483c0c714999db
Ben Evans <bevans@zendesk.com>
2015-05-22 13:32:17 -0500

    slight tweak for readability

--- Ruby/euler1.rb
+++ Ruby/euler1.rb
@@ -1,11 +1,10 @@
 def sumMultiple(start, finish, *multiples)
-    return (start..finish - 1).select{|i| 
-        multiples.any? {|multiple| 
-            i % multiple == 0 }
-        }.reduce(:+)
+    (start..finish - 1).select do |i|
+        multiples.any? do |multiple|
+            i % multiple == 0 
+        end
+    end.reduce(:+)
 end
-
-
 
 puts sumMultiple(1,10,3,5)
 puts sumMultiple(1,1000,3,5)
--- Ruby/euler2.rb
+++ Ruby/euler2.rb
@@ -1,32 +1,45 @@
-def sumMultiple(start, finish, *multiples)
-    return (start..finish - 1).select{|i| 
-        multiples.any? {|multiple| 
+def sumMultiple(seq, *multiples)
+    seq.select{|i|
+        multiples.any? {|multiple|
             i % multiple == 0 }
         }.reduce(:+)
 end
 
-def next_fib(a, b)
-    return a+b
-end
-
-def fib(max)
-    i = 1
-    j = 1
-    arr = []
-    while i < max do
-        arr << next_fib(i, j)
-        j = i
-        i = i + j
+fib = Enumerator.new do |yielder|
+    i1, i2 = 1, 1
+    loop do
+        yielder.yield i1
+        i1, i2 = i2, i1 + i2
     end
-    return arr
 end
 
+puts sumMultiple(fib.take_while {|n| n < 4000000}, 2)
 
+#
+# Learning how ruby deals with iterators and blocks
+#
+# def fib2(n)
+#     i1, i2 = 1, 1
+#     while i1 <= n
+#         yield i1
+#         i1, i2 = i2, i1 + i2
+#     end
+# end
 
+# def fib3()
+#     i1, i2 = 1, 1
+#     loop do
+#         yield i1
+#         i1, i2 = i2, i1 + i2
+#     end
+# end
 
+# fib2(5) {|n| puts n}
 
-
-fib(100).each do |num|
-    puts num
-end 
-# puts sumMultiple(1,1000,3,5)
+# fib3() do |n|
+#     if n > 5
+#         break
+#     else
+#         puts n
+#     end
+# end
